35307b33a6dec2034abffc0440f567ee4f47f2d8
mani <mani5984@gmail.com>
2014-11-08 12:50:16 -0500

    updated coin class

--- Coin Class/coin.h
+++ Coin Class/coin.h
@@ -1,20 +1,32 @@
-#include<iostream>
-#include<string>
+#include <stdio.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+
 using namespace std;
 
 class Coin{
 public:
-  int diameter, value;
+  int diameter;
+  string value;
 
   //Constructor
-  Coin(int cX, int cY){
-    int marker = 0;
-    int i = 0;
-    while(marker != 1){
-      M = get.Matrix
-	marker = M(cX+i,cY);
-      i++;
+  Coin(Mat mask){
+    int size = mask.size;
+    int height = size.height;
+    int width = size.width;
+    bool coinFlag = false;
+    int counter = 0;
+    for(int i = 0; int < height; i++){
+      for(int j = 0; int < width; j++){
+	if(mask(i,j) == 255){
+	  coinFlag = true;
+	  cointer++;
+	}
+      }
+      coinFlag = false;
     }
-    diameter = 2*i+1;
+    diameter = counter;
   }
+
+  value = 'no coin';
 };
