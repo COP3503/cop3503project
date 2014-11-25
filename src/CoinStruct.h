56cfaff4ad39e582967744fe332248e44c352db1
mani <mani5984@gmail.com>
2014-11-24 19:14:51 -0500

    separated coin constructor and class into coin.h and coin.cpp

---
+++ src/CoinStruct.h
@@ -0,0 +1,11 @@
+
+    struct CoinRatios {
+      double Ratio;
+      string CoinA;
+      string CoinB;
+    } Ratios [16];
+
+    double pennyD = .75;
+    double nickelD = .835;
+    double dimeD = .705;
+    double quarterD = .955;
