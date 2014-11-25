91372a4a540f3613e10aa46d7f1bfcb820701aad
brettski <brettnelson@ufl.edu>
2014-11-24 23:59:29 -0500

    Goodnight

--- src/Data.cpp
+++ src/Data.cpp
@@ -9,18 +9,30 @@ void Data::Data(string imgPath) {
     mainImage imread(path);
     binaryArray get_hough_masks(mainImage);
     piggyBank = fillPiggyBank(binary);
+    labeller(piggyBank);
 }
 
 vector<coin> Data::fillPiggyBank(vector<Mat*> binaryArray) {
-    for (Mat* bin : binaryArray) {
+    for ( Mat* bin : binaryArray ) {
         piggyBank.push_back(Coin(bin));
     }
 }
 
 double Data::sumCoins() {
     double value = 0;
-    for (Coin coin) {
+    for ( Coin coin : piggyBank ) {
         value += coin.getDollarValue();
     }
     return value;
+}
+
+double Data::countDenomination(string denom) {
+    double count = 0;
+    for ( Coin coin : piggyBank ) {
+        if (coin.getDenomination().compare(denom)) 
+        {
+            count++;
+        }
+    }
+    return count;
 }
