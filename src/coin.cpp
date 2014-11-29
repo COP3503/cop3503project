67dcc5246837928afb8751b7bf2ab87c3326bfcc
mani <m.angerhofer@ufl.edu>
2014-11-28 19:30:22 -0500

    completed coin.cpp (for now), all debugging messages commented out

--- src/coin.cpp
+++ src/coin.cpp
@@ -10,7 +10,6 @@ using namespace cv;
 
 using namespace std;
 
-
 struct CoinStruct {
   double Ratio;
   double Difference;
@@ -28,16 +27,13 @@ public:
   //order starting from 1: penny, nickel, dime, quarter
   vector<double> allProbabilities[4];
   vector<double> finalProbabilities;
-  vector<double> pProb;
-  vector<double> nProb;
-  vector<double> dProb;
-  vector<double> qProb;
 
-  //map<string,double> diameterMap;
+  map<string,double> diameterMap;
+
   //Constructor
   Coin(Mat mask){
-    int height = mask.cols;
-    int width = mask.rows;
+    int width = mask.cols;
+    int height = mask.rows;
     bool coinFlag = false;
     int counter = 0;
     for(int i = 0; i < height; i++){
@@ -53,7 +49,7 @@ public:
       coinFlag = false;
     }
     diameter = counter;
-    cout<<"diameter = "<<diameter<<endl;
+    //    cout<<"diameter = "<<diameter<<endl;
     denomination = "unknown";
     dollarValue = 0;
     //diameterMap = 
@@ -62,7 +58,7 @@ public:
   void setDenomination(string name){
     //    if (name=="Dollar-coin") a.dollarValue += 1;
     //    if (name=="Half-dollar") a.dollarValue += .5;
-    if (name=="quater") dollarValue += .25;
+    if (name=="quarter") dollarValue += .25;
     if (name=="dime") dollarValue += .1;
     if (name=="nickle") dollarValue +=.05;
     if (name=="penny") dollarValue +=.01;
@@ -77,36 +73,42 @@ public:
     return diameter;
   }
 
+  map<string,double> getDiameterMap(){
+    return diameterMap;
+  }
+
   //Adds a probability of a coin type to its appropriate coin vector
   void updateProbablities(string denomination, double Prob){
-    //    Coin A;
-    //  cout<<"Prob = "<<Prob<<endl;
-    //  cout<<"denomination= "<<denomination<<endl;
-    if (denomination.compare("penny")==1)allProbabilities[1].push_back(Prob);
-    if (denomination.compare("nickel")==1) allProbabilities[2].push_back(Prob);
-    if (denomination.compare("dime")==1) allProbabilities[3].push_back(Prob);
-    if (denomination.compare("quarter")==1)qProb.push_back(Prob);
+    if (denomination.compare("penny")==0) allProbabilities[0].push_back(Prob);
+    if (denomination.compare("nickel")==0) allProbabilities[1].push_back(Prob);
+    if (denomination.compare("dime")==0) allProbabilities[2].push_back(Prob);
+    if (denomination.compare("quarter")==0) allProbabilities[3].push_back(Prob);
   }
 
   //Finds the best probability of each coin type for a coin and adds them to the finalProbabilites vector
   void bestProb(){
+    string name[4] = {"penny","nickel","dime","quarter"};
     double max;
-    for(int i = 1; i<=4; i++){
+    for(int i = 0; i<4; i++){
       int size = allProbabilities[i].size();
-      //    cout<<"sizeAll = "<<size<<endl;
-      for(int j = 1; j<=size-1; j++){
-	//	cout<<"prob j = "<<allProbabilities[i].at(j-1)<<endl;
-	//	cout<<"prob j+1 = "<<allProbabilities[i].at(j)<<endl;
-	//	cout<<"j = "<<j<<endl;
-	if(allProbabilities[i].at(j-1) > allProbabilities[i].at(j)){
-	  max = allProbabilities[i].at(j-1);
-	}
-	else{
-	  max = allProbabilities[i].at(j);
-	}
+      if(size == 0){
+	max = 0;
       }
-      finalProbabilities.push_back(max);
-      cout<<"pushed "<<max<<" for "<<i<<endl;
+      else{
+	max = allProbabilities[i].at(0);
+	for(int j = 1; j<=size-1; j++){
+	  //	cout<<"prob j = "<<allProbabilities[i].at(j-1)<<endl;
+	  //	cout<<"prob j+1 = "<<allProbabilities[i].at(j)<<endl;
+	  //	cout<<"j = "<<j<<endl;
+
+	  if(allProbabilities[i].at(j) > max){
+	    max = allProbabilities[i].at(j);
+	  }
+	}
+	finalProbabilities.push_back(max);
+	diameterMap[name[i]]=max;
+	//	cout<<"pushed "<<max<<" for "<<i<<endl;
+      }
     }
   }
 
@@ -114,12 +116,22 @@ public:
   void printProbs(){
     for(int i = 1; i<=4; i++){
       int size = allProbabilities[i].size();
-      cout<<"Coin"<<i<<endl;
+      //      cout<<"all Coin"<<i<<endl;
       for(int j = 1; j<=size; j++){
-	cout<<"index"<<j<<" = "<<allProbabilities[i].at(j-1)<<endl;
+	//	cout<<"index"<<j<<" = "<<allProbabilities[i].at(j-1)<<endl;
       }
-      cout<<endl;
+      //      cout<<endl;
     }
+
+    //    cout<<endl;
+    //    cout<<endl;
+
+    int size2 = finalProbabilities.size();
+    //    cout<<"final Coin"<<endl;
+    for(int j = 1; j<=size2; j++){
+      //      cout<<"index"<<j<<" = "<<finalProbabilities.at(j-1)<<endl;
+    }
+    //    cout<<endl;
   }
 
   //Compare 2 coins and find relative confidence
@@ -140,7 +152,7 @@ public:
     double dA = diameter;
     double dB = B.getDiameter();
     double ratio = dA/dB;
-    cout<<"ratio = "<<ratio<<endl;
+    //    cout<<"ratio = "<<ratio<<endl;
     double scaledDifference = abs(dA-dB)/((dA+dB)/2);
     double sProb, rProb, aProb;
 
@@ -148,9 +160,12 @@ public:
       if(Ideal[i].Ratio != 1){
 	rProb = 1 - abs(ratio - Ideal[i].Ratio);
 	sProb = 1 - abs(scaledDifference - Ideal[i].ScaledDifference);
-	aProb = (rProb + sProb)/2;
+	aProb = abs((rProb + sProb)/2);
 	updateProbablities(Ideal[i].CoinA,aProb);
 	B.updateProbablities(Ideal[i].CoinB,aProb);
+	//	cout<<"coinA pass = "<<Ideal[i].CoinA<<endl;
+	//	cout<<"coinB pass = "<<Ideal[i].CoinB<<endl;
+	//	cout<<endl;
       }
     }
   }
@@ -169,8 +184,8 @@ void compareCoins(Coin array[], int size){
   }
 
   for(int i = 0; i<size; i++){
-    array[i].printProbs();
     array[i].bestProb();
+    array[i].printProbs();
   }
 }
 
@@ -181,31 +196,30 @@ int main(){
   Mat img2 = imread("/home/mani/School/UF/Fall 2014/COP3503/Final Project/cop3503project/test/coins_1_mask_2.png");
   Mat img3 = imread("/home/mani/School/UF/Fall 2014/COP3503/Final Project/cop3503project/test/coins_1_mask_3.png");
 
-  //  Coin* test = new Coin(img1);
+  /*  int height = img1.cols;
+  int width = img1.rows;
+
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j < width; j++){
+      cout<<img1.at<int>(i,j);
+      }
+    }
+  }*/
 
   Coin coinArray[3]{{img1},{img2},{img3}};
 
-  /*  Mat img;
-      img = imread("/home/mani/School/UF/Fall 2014/COP3503/Final Project/cop3503project/test/coins_1_mask_1.png");
-      Coin coinArray[1] = new Coin(img);
-      img imread("/home/mani/School/UF/Fall 2014/COP3503/Final Project/cop3503project/test/coins_1_mask_2.png");
-      Coin coinArray[2] = new Coin(img)
-      img imread("/home/mani/School/UF/Fall 2014/COP3503/Final Project/cop3503project/test/coins_1_mask_3.png");
-      Coin coinArray[3] = new Coin(img);*/
-
-  cout<<"Coin Probs"<<endl; 
-
   compareCoins(coinArray,3);
 
-  for(int i = 1; i<=3; i++){
-     cout<<"Coin"<<i<<" Probs"<<endl; 
+  for(int i = 0; i<3; i++){
+    cout<<"Coin"<<i<<" Probs"<<endl; 
 
-    cout<<"size = "<<coinArray[i].finalProbabilities.size()<<endl;
+    //cout<<"size = "<<coinArray[i].finalProbabilities.size()<<endl;
 
-    cout<<"Penny% = "<<coinArray[i].finalProbabilities.front()<<endl;
-    cout<<"Nickel% = "<<coinArray[i].finalProbabilities.at(2)<<endl;
-    cout<<"Dime% = "<<coinArray[i].finalProbabilities.at(3)<<endl;
-    cout<<"Quarter% = "<<coinArray[i].finalProbabilities.at(4)<<endl;
+    cout<<"Penny% = "<<coinArray[i].finalProbabilities.at(0)<<endl;
+    cout<<"Nickel% = "<<coinArray[i].finalProbabilities.at(1)<<endl;
+    cout<<"Dime% = "<<coinArray[i].finalProbabilities.at(2)<<endl;
+    cout<<"Quarter% = "<<coinArray[i].finalProbabilities.at(3)<<endl;
+    cout<<endl;
   }
   return 0;
 }
