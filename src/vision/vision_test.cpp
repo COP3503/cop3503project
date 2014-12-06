fcf3e1c7920db20a801afb19d052c53869ec18b1
minggit <yang19500@ufl.edu>
2014-12-05 21:31:12 -0500

    previous changes

--- src/vision/vision_test.cpp
+++ src/vision/vision_test.cpp
@@ -17,9 +17,29 @@ int main(int argc, char* argv[]) {
   Mat input_image;
   input_image = imread("../../test/coins_1.jpg", CV_LOAD_IMAGE_COLOR);
 
-  // Again, should be using smart ptrs
-  // get_hsv_masks(input_image);
-  // imshow("Test Image", input_image);  
+  imshow("lalala", input_image);
+
+/*
+  Mat input_image_HSV, input_image_HSVmask_Copper, input_image_HSVmask_Silver;
+
+
+  cvtColor(input_image, input_image_HSV, CV_BGR2HSV);
+  // adaptiveThreshold(InputArray src, OutputArray dst, double maxValue, int adaptiveMethod, int thresholdType, int blockSize, double C)
+
+  //inrange for copper
+  inRange(input_image_HSV, Scalar(cop_H_Low, cop_S_Low, cop_V_Low), Scalar(cop_H_High, cop_S_High, cop_V_High),input_image_HSVmask_Copper);
+
+  //inragne for silver
+  
+
+  imshow("copper color mask", input_image_HSV);
+*/
+
+
+/*
+  Again, should be using smart ptrs
+  get_hsv_masks(input_image);
+  imshow("Test Image", input_image);  
 
   vector<Mat*> masks = get_hough_masks(input_image);
   std::cout << masks.size() << std::endl;
@@ -41,6 +61,6 @@ int main(int argc, char* argv[]) {
    };
    
    std::cout << "Test Result: " << label(sampleInput) << std::endl;
-   
+   */
   return 0;
 }
